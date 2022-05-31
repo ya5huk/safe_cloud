fd8c0480e87d9b25b0f48a79ab1f1e7b8344b4bb
Ilan Yashuk <ilan147963@gmail.com>
2022-05-31 18:30:22 +0300

    Window Management + Files can be dropped now

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
-from kivy.app import App
-from kivy.uix.label import Label 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from IconGrabber import IconGrabber
+import os
 
-# Keep watching -> https://www.youtube.com/watch?v=yg7n8hP6k1w
+ig = IconGrabber('images/extension_icons/')
 
-class SafeCloudApp(App):
+class LoginWindow(Screen):
+    pass
+
+class RegisterWindow(Screen):
+    pass
+
+class CloudWindow(Screen):
+    def __init__(self, **kw):
+        super(CloudWindow, self).__init__(**kw)
+        Window.bind(on_drop_file=self.file_dropped)
+        Window.clearcolor = (73/255, 140/255, 125/255, 1)
+        self.files_counter = 0
+
+    def file_dropped(self, window, filename, x, y, *args):
+        
+        showed_filename = self.prepare_filename(filename.decode())
+        bl = BoxLayout(spacing=5, size_hint=(.1,.2), orientation="vertical")
+        bl.add_widget(Image(source=ig.grab_filepath(filename.decode()), keep_ratio=True, size_hint=(1,.5)))
+        bl.add_widget(Label(font_name="lbrite", text=showed_filename, halign="center", valign="top", text_size = (bl.size[0], None),size_hint = (1, .5)))
+        self.ids.files_stack.add_widget(bl)
+        
+        self.files_counter += 1
+    
+    @staticmethod
+    def prepare_filename(filename: str):
+        # Function for shortening too long filenames!
+        # Allowing max 20 chars
+        # if more -> 17 - len(ext) chars are showed
+        orig_filename = os.path.basename(filename)
+        if len(orig_filename) > 20:
+            ext = orig_filename.split('.')[-1]
+            showed_filename = orig_filename[:20-3-len(ext)] + '...' + ext
+            return showed_filename
+        return orig_filename
+        
+class WindowManager(ScreenManager):
+    pass
+
+
+class SafeCloudApp(MDApp):
+    def __init__(self, **kwargs):
+        self.title = "Safe Cloud"
+        super().__init__(**kwargs)
+
     def build(self):
-        return FloatLayout()
+        self.root = Builder.load_file("safecloud.kv")
 
 if __name__ == '__main__':
     SafeCloudApp().run()
