17fd8277f041de7bede80a508c4453ace0d63ad3
Ilan Yashuk <ilan147963@gmail.com>
2022-06-02 13:48:38 +0300

    A bit of progression

--- DBCommands.py
+++ DBCommands.py
@@ -1,5 +1,27 @@
 import sqlite3
 
+from DBObjects import DBFile
+
 class DBCommands:
     def __init__(self, db_file_path: str):
         self.db_file_path = db_file_path
+        self.db_con = sqlite3.connect(db_file_path)
+        self.cur = self.db_con.cursor() # Will be used to operate in db
+    
+    def create_tables(self):
+        self.cur.execute('''CREATE TABLE files
+        (file_id integer primary key autoincrement, in_dir integer, name text, content blob, added_date text)''')
+
+        # ...
+        self.cur.commit()
+    
+    def add_file(self, file: DBFile):
+        self.cur.execute(f'''INSERT INTO files
+        ({file.file_id}, {file.in_dir}, {file.name}, {file.content}, {str(file.added_date)})''')
+
+    def close(self):
+        self.cur.close()
+
+if __name__ == "__main__":
+    db = DBCommands()
+    db
--- DBObjects.py
+++ DBObjects.py
@@ -1,15 +1,20 @@
+import datetime
+
 class DBFile:
-    def __init__(self, in_dir: bool, name: str, content: bytes):
+    def __init__(self, file_id: int, in_dir: bool, name: str, content: bytes, added_date: datetime):
+        self.file_id = file_id
         self.in_dir = in_dir
         self.name = name
         self.content = content
+        self.added_date = added_date
+
+    # def to_dict(self):
+    #     return {
+            
+    #         'in_dir': self.in_dir,
+    #         'name': self.name,
+    #         'content': self.content
+    #     }
     
-    def to_dict(self):
-        return {
-            'in_dir': self.in_dir,
-            'name': self.name,
-            'content': self.content
-        }
-    
-    def __repr__(self):
-        return f'{self.name}, {" in dir" if self.in_dir else " not in dir"}'
+    # def __repr__(self):
+    #     return f'{self.name}, {" in dir" if self.in_dir else " not in dir"}'
