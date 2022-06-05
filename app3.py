c41dd2fe22adb3dfd00bdcbea89246210abb3da1
Ilan Yashuk <ilan147963@gmail.com>
2022-06-05 13:29:48 +0300

    Added register page

--- app3.py
+++ app3.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
